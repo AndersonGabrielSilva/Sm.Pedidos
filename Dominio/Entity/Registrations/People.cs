bef26c6d68b48a576ce939b11df1715e935f45c9
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-09-11 18:17:16 -0300

    Bacckup

--- Dominio/Entity/Registrations/People.cs
+++ Dominio/Entity/Registrations/People.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Domain.Entity.Registrations
 {
-    public class People
+    public class People : EntityBase
     {
         public string Name { get; set; }
         public string LastName { get; set; }
